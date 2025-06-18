134a69a9f1efb0436bb8059092cb6b1ac9e4cff2
GraceKayem <11557@holbertonstudents.com>
2025-06-18 14:35:00 +0000

    save

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - 5-printf.c
+ * Description: Write a C program that prints the quote in question 5
+ * Return: Needs to return 0 value
+ */
+
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+}
+
