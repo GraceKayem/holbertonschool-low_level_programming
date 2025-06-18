882e3d13edd9aca4f33f1485eafa62834d3030f5
GraceKayem <11557@holbertonstudents.com>
2025-06-18 14:13:44 +0000

    save

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * Needs to return 0 value 
+ * Needs to return 0 value
  */
 
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+return (0);
 }
