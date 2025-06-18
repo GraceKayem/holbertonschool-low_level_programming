b2cc7167901d8f7c850fb404c8cc8b18e85fa732
GraceKayem <11557@holbertonstudents.com>
2025-06-18 15:35:29 +0000

    save

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - 6-size.c
  * Description: You should produce the exact same output as in the example
- * Return: should return 0 
+ * Return: should return 0
  */
 
 int main(void)
@@ -13,6 +13,6 @@ printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
-    
+
 return (0);
 }
