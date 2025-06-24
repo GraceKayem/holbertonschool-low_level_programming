6dc63c380f6d1f38ce36ed7bf04f70eb07fe2d88
GraceKayem <11557@holbertonstudents.com>
2025-06-24 13:46:38 +0000

    save

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/* 
+ *main- 0-positive_or_negative
+ *Description - If statement n
+*Return: Always 0 (sucess)
+*/
 int main(void)
 {
 int n;
