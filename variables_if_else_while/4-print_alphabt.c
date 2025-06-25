eeba54b8eb7266b7070456e3406c38e774f12101
GraceKayem <11557@holbertonstudents.com>
2025-06-25 04:27:16 +0000

    save

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -2,11 +2,18 @@
 /**
  * main - Entry point of the program
  *
- * Description:  *
+ * Description: print_alphabet
+ *
  * Return: Always 0 (Success)
  */
 
-int main(void) {
- 
-  return 0;
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
 }
