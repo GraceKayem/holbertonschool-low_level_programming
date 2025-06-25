c80da6b933c06b1f73f82ff65d0b319068212d01
GraceKayem <11557@holbertonstudents.com>
2025-06-25 03:16:47 +0000

    save

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,8 +7,14 @@
  * Return: Always 0 (Success)
  */
 
-int main(void) {
-for 
-putcher('A');
-return 0;
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+return (0);
 }
