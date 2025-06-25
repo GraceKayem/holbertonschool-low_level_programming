b90c507eefc261c035148573af388cb5d3fff952
GraceKayem <11557@holbertonstudents.com>
2025-06-25 02:01:09 +0000

    save

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: print_alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void) {
+for 
+putcher('A');
+return 0;
+}
