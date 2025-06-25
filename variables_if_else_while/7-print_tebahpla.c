d9cf5180fda0522875ccdc160fbb6fa0614424fc
GraceKayem <11557@holbertonstudents.com>
2025-06-25 11:20:20 +0000

    save

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description:  *
+ * Return: Always 0 (Success)
+ */
+
+int main(void) {
+int alph_rev;
+
+for (alph_rev = 'a'; alph_rev <= 'z'; alph_rev++)
+{
+putchar(alph_rev);
+}
+putchar('\n');
+return (0);
+}
