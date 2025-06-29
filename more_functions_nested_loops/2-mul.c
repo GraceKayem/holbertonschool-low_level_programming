28bc3e8773ac4f5ccd4f137e223c6218a1c1e44c
GraceKayem <11557@holbertonstudents.com>
2025-06-29 10:32:23 +0000

    save

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Entry point of the program
+ *
+ * @a: function that multiplies two integers
+ * @b: function that multiplies two integers
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
