d305d44e5e7f349a4efa14c1f0f60ec36f93e6f8
GraceKayem <11557@holbertonstudents.com>
2025-06-29 10:53:52 +0000

    save

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,10 @@
  *
  * Return: Always 0 (Success)
  */
-void more_numbers(void) {
+void more_numbers(void)
+{
 int lines;
-Int num;
+int num;
 for (lines = 0; lines < 10; lines++)
 {
 for (num = 0; num <= 14; num++)
