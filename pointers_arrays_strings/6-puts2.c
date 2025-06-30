0a76efd6b159323d38161c79a043c0366fa6ce6f
GraceKayem <11557@holbertonstudents.com>
2025-06-30 12:36:17 +0000

    save

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point of the program
+ *
+ * @str: function that prints every other character of a string
+ *
+ * Return: Always 0 (Success)
+ */
+void puts2(char *str)
+{
+int firstChar = 0;
+while (str[firstChar] != '\0')
+{
+_putchar(str[firstChar]);
+firstChar  += 2;
+}
+_putchar ('\n');
+}
