78a837bf6ea969dc8e5285166857f48c647bf79c
GraceKayem <11557@holbertonstudents.com>
2025-07-06 07:10:33 +0000

    save

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * @argc: prints all arguments it receives
+ * @argv: prints all arguments it receives
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%d: %s\n", i, argv[i]);
+}
+printf("\n");
+return (0);
+}
