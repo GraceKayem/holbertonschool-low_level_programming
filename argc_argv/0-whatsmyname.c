64dcc2d4760c13c622d6e39deb32a1da848ce18c
GraceKayem <11557@holbertonstudents.com>
2025-07-06 06:53:37 +0000

    save

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * @argc: program that prints its name
+ * @argv: program that prints its name
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+
+printf("%s\n", argv[0]);
+printf("\n");
+return (0);
+}
