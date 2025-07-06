fe68f8ef6f98ea73226f57e30fcd3042f8834cdb
GraceKayem <11557@holbertonstudents.com>
2025-07-06 06:34:27 +0000

    save

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -12,11 +12,11 @@ void _puts_recursion(char *s)
 {
 if (*s == '\0') /*se case so its not infinite*/
 {
-putchar ('\n');
+_putchar ('\n');
 return;
 }
 /*print current character h from hello*/
-putchar(*s);
+_putchar(*s);
 /*recursive for the next character o from hello*/
 _puts_recursion(s + 1);
 }
