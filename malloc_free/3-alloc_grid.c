73933af2e328a0ac927b817a036465e4ccf1266c
GraceKayem <11557@holbertonstudents.com>
2025-07-10 15:05:19 +0000

    save

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -17,15 +17,11 @@ int i;
 int j;
 
 if (width <= 0 || height <= 0)
-{
 return (NULL);
-}
 
 arr = malloc(height * sizeof(int *));
 if (arr == NULL)
-{
 return (NULL);
-}
 
 for (i = 0; i < height; i++)
 {
@@ -33,16 +29,14 @@ arr[i] = malloc(width * sizeof(int));
 if (arr[i] == NULL)
 {
 for (j = 0; j < i; j++)
-{
 free(arr[j]);
-}
 free(arr);
 return (NULL);
 }
-}
+
 for (j = 0; j < width; j++)
-{
 arr[i][j] = 0;
 }
+
 return (arr);
 }
