8d10182e763f87fbf9ad37f8528dd2164c8d85f7
GraceKayem <11557@holbertonstudents.com>
2025-07-10 12:40:24 +0000

    save

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,1 +1,13 @@
-malloc_free README directory
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * @argc:
+ * @argv:
+ *
+ * Return: Always 0 (Success)
+ */
+
