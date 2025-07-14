64e3ef77c40a82820602ac6208b721bb85e1a002
GraceKayem <11557@holbertonstudents.com>
2025-07-14 04:50:38 +0000

    save

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Entry point of the program
+ *
+ * @name: Define a new type struct dog
+ * @age: Define a new type struct dog
+ * @owner: Define a new type struct dog
+ *
+ * Return: Always 0 (Success)
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
