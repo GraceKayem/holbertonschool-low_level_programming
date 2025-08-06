fc1260137889e58fa324136f0aa428935cad2d0f
GraceKayem <11557@holbertonstudents.com>
2025-08-06 16:35:15 +0000

    save

--- file_io/main.h
+++ file_io/main.h
@@ -5,6 +5,8 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -12,7 +14,7 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void error_exit(int exit_code, const char *message, const char *arg);
 int main(int ac, char **av);
-void close_fd(int fd)
+void close_fd(int fd);
 
 
 #endif
