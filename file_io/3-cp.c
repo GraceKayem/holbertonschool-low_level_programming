086fd8dd7486cc3c0ab9d542ce45afb763480fc8
GraceKayem <11557@holbertonstudents.com>
2025-08-06 15:58:39 +0000

    save

---
+++ file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * @ac: program that copies the content of a file to another file
+ * @av: program that copies the content of a file to another file
+ *
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to;
+	int r, w;
+	char buffer[BUFFER_SIZE];
+
+	if (ac != 3)
+	error_exit(97, "Usage: %s file_from file_to\n", av[0]);
+
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+	error_exit(98, "Error: Can't read from file %s\n", av[1]);
+
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close(fd_from);
+		error_exit(99, "Error: Can't write to %s\n", av[2]);
+	}
+	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+	w = write(fd_to, buffer, r);
+		if (w == -1 || w != r)
+		{
+			close(fd_from);
+			close(fd_to);
+			error_exit(99, "Error: Can't write to %s\n", av[2]);
+		}
+	}
+	if (r == -1)
+	{
+		close(fd_from);
+		close(fd_to);
+		error_exit(98, "Error: Can't read from file %s\n", av[1]);
+	}
+	if (close(fd_from) == -1)
+	error_exit(100, "Error: Can't close fd %d\n", av[1]);
+
+	if (close(fd_to) == -1)
+	error_exit(100, "Error: Can't close fd %d\n", av[2]);
+
+	return (0);
+}
+
+#define BUFFER_SIZE 1024
+
+/**
+ * error_exit - program that copies the content of a file to another file
+ * @exit_code: program that copies the content of a file to another file
+ * @message: program that copies the content of a file to another file
+ * @arg: program that copies the content of a file to another file
+ */
+void error_exit(int exit_code, const char *message, const char *arg)
+{
+	dprintf(STDERR_FILENO, message, arg);
+	exit(exit_code);
+}
+
--- file_io/main.h
+++ file_io/main.h
@@ -7,9 +7,11 @@
 #include <stdlib.h>
 
 
-
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_exit(int exit_code, const char *message, const char *arg);
+int main(int ac, char **av);
+
 
 #endif
